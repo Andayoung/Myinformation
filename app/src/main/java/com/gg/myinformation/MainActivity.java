31b6bf7940b1d3a2891037dcd1496435db2634c9
zhoup <zhoup324@163.com>
2017-07-17 15:23:44 +0800

    Initial commit

---
+++ app/src/main/java/com/gg/myinformation/MainActivity.java
@@ -0,0 +1,111 @@
+package com.gg.myinformation;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.user_name_uu)
+    TextView userNameUu;
+    @BindView(R.id.user_pwd_uu)
+    TextView userPwdUu;
+    @BindView(R.id.user_bir_uu)
+    TextView userBirUu;
+    @BindView(R.id.user_sex_uu)
+    TextView userSexUu;
+    @BindView(R.id.btn_exit)
+    Button btnExit;
+    private SerialNumberHelper serialNumberHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        if (!checkIsLogin()) {
+            Intent intent = new Intent(MainActivity.this, LogOrRegActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (serialNumberHelper == null) {
+            serialNumberHelper = new SerialNumberHelper(getApplicationContext());
+        }
+        String serialNumber = serialNumberHelper.read4File();
+        Log.e("MainActivity", "serialNumber=" + serialNumber);
+        if (serialNumber == null || serialNumber.equals("")) {
+        } else {
+            String[] s = serialNumber.split(" ");
+            userNameUu.setText(s[2]);
+            userPwdUu.setText(s[3]);
+            userBirUu.setText("1998.08.08");
+            userSexUu.setText("女");
+        }
+    }
+
+    private boolean checkIsLogin() {
+        if (serialNumberHelper == null) {
+            serialNumberHelper = new SerialNumberHelper(getApplicationContext());
+        }
+        String serialNumber = serialNumberHelper.read4File();
+        Log.e("MainActivity", "serialNumber=" + serialNumber);
+        if (serialNumber == null || serialNumber.equals("")) {
+            return false;
+        } else {
+            String[] s = serialNumber.split(" ");
+            userNameUu.setText(s[2]);
+            userPwdUu.setText(s[3]);
+            userBirUu.setText("1998.08.08");
+            userSexUu.setText("女");
+            /*int i = 0;
+            for (String z : s) {
+                Log.e("MainActivity", "s[" + (i++) + "]=" + z);
+            }*/
+            return true;
+        }
+
+    }
+
+    @OnClick({R.id.btn_exit})
+    void exit(Button btn) {
+        if (serialNumberHelper == null) {
+            serialNumberHelper = new SerialNumberHelper(getApplicationContext());
+        }
+        serialNumberHelper.deleteFile();
+        finish();
+
+    }
+
+    void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("还没有登录？");
+        builder.setTitle("提示");
+        builder.setPositiveButton("退出", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                MainActivity.this.finish();
+            }
+        });
+        builder.setNegativeButton("去登录", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
+
+}
---
+++ app/src/main/java/com/gg/myinformation/SerialNumberHelper.java
@@ -0,0 +1,84 @@
+package com.gg.myinformation;
+
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by Administrator on 2017/6/22 0022.
+ */
+
+public class SerialNumberHelper {
+    private Context context;
+
+    public SerialNumberHelper() {
+    }
+
+    public SerialNumberHelper(Context context) {
+        super();
+        this.context = context;
+    }
+
+    public void save2File(String text) {
+        try {
+            File path= Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            if(!path.mkdirs()) {
+                Log.e("licence", "Directory not created");
+            }
+            File file=new File(path,"my.uu");
+            FileOutputStream output = new FileOutputStream(file);
+            output.write(text.getBytes("utf-8"));
+            output.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public String read4File() {
+        StringBuilder sb = new StringBuilder("");
+        try {
+            File path=Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            if(!path.mkdirs()) {
+                Log.e("licence", "Directory not created");
+            }
+            File file=new File(path, "my.uu");
+            FileInputStream input = new FileInputStream(file);
+            byte[] temp = new byte[1024];
+            int len = 0;
+            while ((len = input.read(temp)) > 0) {
+                sb.append(new String(temp, 0, len));
+            }
+            input.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+
+    public void deleteFile(){
+        File path= Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        if(!path.mkdirs()) {
+            Log.e("licence", "Directory not created");
+        }
+        File file=new File(path, "my.uu");
+        if(file.isFile()&&file.exists()){
+            file.delete();
+        }
+    }
+
+}
